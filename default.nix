900699f3cc439231f8af8ac9e1c56243bfcc46ce
LucasNT <lucasntakahashi@gmail.com>
2025-06-13 07:10:33 -0300

    feat(flake): add flake to build package

---
+++ default.nix
@@ -0,0 +1,11 @@
+{ stdenv, lib, path, pkgs, libnotify }:
+
+pkgs.buildGoModule {
+  pname = "MyFeedNotification";
+  version = "v1.0";
+
+  src = path;
+  buildInputs = [ libnotify ];
+
+  vendorHash = "sha256-zZLQ2laQTjB6bFfrhqtnFSWwJCd1Gb+0LmPc5a3rtro=";
+}
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      packages.x86_64-linux.my_feed_notification =
+        pkgs.callPackage ./default.nix { path = "${self}"; };
+      devShell.x86_64-linux = pkgs.callPackage ./default.nix {
+        path = "${self}";
+        dev-shell = true;
+      };
+      packages.x86_64-linux.default =
+        self.packages.x86_64-linux.my_feed_notification;
+
+    };
+}
